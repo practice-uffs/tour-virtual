7514bb76a6b8bc84318f1dfdd435961dc4c3fe3c
Ryan <tencatenryan11@gmail.com>
2022-11-16 10:49:10 -0300

    :sparkles: feat: Angulo na info hover

--- scripts/SVGactions.js
+++ scripts/SVGactions.js
@@ -5,16 +5,14 @@ function actionOnHover(target, group, texto,pan){
 
   paper.hover(criar_hover, deletar_hover);
   var points = SVGTarget.getBBox();
-  let angle = Snap.angle(points.x,points.y,points.x2,points.y2) - 250;
 
-  //let angle = Snap.angle(points.x,points.y,0,0);
-  //let angle = Snap.atan( ((points.y2/ points.x2) )) 
-  //let angle = Snap.acos( points.r0) 
-  //console.log(angle);
-  console.log(points);
-  console.log("V:" + points.y/ points.y2);
-  console.log(angle)
-
+  let angle = (points.r1 + points.r2)  - 90; 
+  
+  
+  if((points.r1 + points.r2)> 90){
+    angle = angle * -1;
+  }
+  
 
 function criar_click(){
     let position_card = Snap.format("m {x} {y}c0 -4.586426 3.7180328 -8.304459 8.304474 -8.304459l19.873993 0l0 0l42.26773 0l90.32022 0c2.2024841 0 4.314728 0.8749237 5.8721313 2.432312c1.557373 1.5573883 2.432312 3.6696625 2.432312 5.8721466l0 20.761154l0 0l0 12.456696l0 0c0 4.586426 -3.7180176 8.304459 -8.304443 8.304459l-90.32022 0l-21.1333 6.228348l-21.13443 -6.228348l-19.873993 0c-4.586441 0 -8.304474 -3.7180328 -8.304474 -8.304459l0 0l0 -12.456696l0 0z",
@@ -32,7 +30,10 @@ function criar_click(){
 
 
   function criar_hover(){
-
+  
+  console.log(points);
+  console.log("Angulo:" +angle)
+  console.log("P:"+ (points.r1 + points.r2))
     //Texto
     let text_1 = paper.text(points.x , points.y , texto);
     
@@ -110,6 +111,16 @@ var atributos = {
     "id":"#bloco2",
     "group":"#Gbloco2",
     "texto": "Bloco2"
+  },
+  3:{
+    "id":"#bloco3",
+    "group":"#Gbloco3",
+    "texto": "Bloco3"
+  },
+  4:{
+      "id":"#bloco4",
+      "group":"#Gbloco4",
+      "texto": "Bloco4"
   }
 }
 
