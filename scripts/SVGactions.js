f1e109397c792669108bde8fe27e4d46edc00c36
Ryan <tencatenryan11@gmail.com>
2022-11-16 21:54:02 -0300

    :recycle: refactor: Info

--- scripts/SVGactions.js
+++ scripts/SVGactions.js
@@ -1,59 +1,38 @@
-function actionOnHover(target, group, texto,pan){
-  let label, card;
-  let paper = group;
-  let SVGTarget = target;
-
-  paper.hover(criar_hover, deletar_hover);
-  var points = SVGTarget.getBBox();
-
-  let angle = (points.r1 + points.r2)  - 90; 
+class Info{
   
+  constructor(SVGtarget, group){
+    this.paper = group;
+    this.target = SVGtarget;
+    this.label = null;
+    this.points = this.target.getBBox();
+    this.angle = (this.points.r1 + this.points.r2)  - 90; 
   
-  if((points.r1 + points.r2)> 90){
-    angle = angle * -1;
-  }
-  angle = 0;
-  
-
-function criar_click(){
-    let position_card = Snap.format("m {x} {y}c0 -4.586426 3.7180328 -8.304459 8.304474 -8.304459l19.873993 0l0 0l42.26773 0l90.32022 0c2.2024841 0 4.314728 0.8749237 5.8721313 2.432312c1.557373 1.5573883 2.432312 3.6696625 2.432312 5.8721466l0 20.761154l0 0l0 12.456696l0 0c0 4.586426 -3.7180176 8.304459 -8.304443 8.304459l-90.32022 0l-21.1333 6.228348l-21.13443 -6.228348l-19.873993 0c-4.586441 0 -8.304474 -3.7180328 -8.304474 -8.304459l0 0l0 -12.456696l0 0z",
-    {
-      x: points.x,
-      y: points.y
+    if(( this.points.r1 + this.points.r2 ) > 90){
+      this.angle *= -1;
     }
-    );
-    card = paper.path(position_card);
-    card.attr({"fill":"black"});
-    card.transform('r' + angle)
-    card = paper.g(card);
-    paper.append(card);
-}
+    this.angle = 0; // Provisório
+  }
 
-
-  function criar_hover(){
-  
-  console.log(points);
-  console.log("Angulo:" +angle)
-  console.log("P:"+ (points.r1 + points.r2))
-    //Texto
-    let text_1 = paper.text(points.x , points.y , texto);
-    
+  #getText(texto){
+    let text_1 = this.paper.text(this.points.x , this.points.y , texto);
     text_1.attr({
       'font-size': '40px',
       'fill': 'white',
       'opacity': 0.1,
     });
     text_1.animate({ opacity: 1 }, 800 );
-  
-    // Card
+    return text_1;
+  }
+
+  #getCard(){
     let position_card = Snap.format("M{x} {y}c0 -4.586426 3.7180328 -8.304459 8.304474 -8.304459l19.873993 0l0 0l42.26773 0l90.32022 0c2.2024841 0 4.314728 0.8749237 5.8721313 2.432312c1.557373 1.5573883 2.432312 3.6696625 2.432312 5.8721466l0 20.761154l0 0l0 12.456696l0 0c0 4.586426 -3.7180176 8.304459 -8.304443 8.304459l-90.32022 0l-21.1333 6.228348l-21.13443 -6.228348l-19.873993 0c-4.586441 0 -8.304474 -3.7180328 -8.304474 -8.304459l0 0l0 -12.456696l0 0z",
     {
-      x: points.x - (points.x) * 0.025,
-      y: points.y - (points.y) *0.02
+      x: this.points.x - (this.points.x) * 0.025,
+      y: this.points.y - (this.points.y) *0.02
     }
     );
 
-    card = paper.path(position_card);
+    let card = this.paper.path(position_card);
     card.attr({
       "fill":"#04923F",
       "stroke": "#08523F",
@@ -61,35 +40,42 @@ function criar_click(){
     });
     
     card.animate({stroke: "#04923F"},500, mina.bounce)
-  
-    // Agroup
-    label = paper.g(card,text_1);
-    label.attr({ opacity: 0.1 });
-    label.animate({ opacity: 1 }, 500 );
-    label.transform('r' + angle);
-    paper.append(label);
+    return card;
   }
 
-  function deletar_hover(){
-      console.log(paper);
-      label.remove(0);
-      
+  #getAgroup(card, SVG_text){
+    this.label = this.paper.g(card,SVG_text);
+    this.label.attr({ opacity: 0.1 });
+    this.label.animate({ opacity: 1 }, 500 );
+    this.label.transform('r' + this.angle);
+    this.paper.append(this.label);
   }
+
+  criar(texto){
+    let SVG_text = this.#getText(texto);
+    let card = this.#getCard();
+    this.#getAgroup(card,SVG_text);
+    
+  }
+
+  deletar(){
+    this.label.remove(0);
+
+  }
+
 }
+
 
 function loadImage(elementos){
   window.onload = function(){
     let s = Snap("#viewport");
     Snap.load("./img/svg/main.svg", onSVGLoaded);
-
     function onSVGLoaded(data) {
           s.append( data );
           for( let elemento in elementos){
             console.log(elementos[elemento].id)
-            setActions(s, elementos[elemento].id, elementos[elemento].group, elementos[elemento].texto);
-          }
-          
-          
+            setActions(s, elementos[elemento].id, elementos[elemento].group, elementos[elemento].texto);     
+          }                
     }
   }  
 
@@ -98,7 +84,16 @@ function loadImage(elementos){
 function setActions(s, id_element, id_group, text){
   let elemento = s.select(id_element);
   let grupo = s.select(id_group);
-  actionOnHover(elemento, grupo,text);
+  let info = new Info(elemento,grupo);
+
+  // Hover
+  grupo.hover(function(){
+    info.criar(text)
+  }, 
+  function(){
+    info.deletar()
+  }); // Fim Hover
+
 }
 
 
@@ -111,41 +106,7 @@ for(let i = 1; i <= tam; i++){
     "texto": "Bloco" + i
   }
 }
-/*
-var atributos = {
-  1:{
-    "id":"#bloco1",
-    "group":"#Gbloco1",
-    "texto": "Bloco1"
-  },
-  2:{
-    "id":"#bloco2",
-    "group":"#Gbloco2",
-    "texto": "Bloco2"
-  },
-  3:{
-    "id":"#bloco3",
-    "group":"#Gbloco3",
-    "texto": "Bloco3"
-  },
-  4:{
-      "id":"#bloco4",
-      "group":"#Gbloco4",
-      "texto": "Bloco4"
-  },
-  5:{
-    "id":"#bloco5",
-    "group":"#Gbloco5",
-    "texto": "Bloco5"
-  },
-  6:{
-    "id":"#bloco6",
-    "group":"#Gbloco6",
-    "texto": "Bloco6"
-  },
 
-}
-*/
 loadImage(atributos);
 
 
