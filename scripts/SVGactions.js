0591c30abe152abfe930323956e991cf27dd18f1
Ryan <tencatenryan11@gmail.com>
2022-11-23 02:25:59 -0300

    feat: jQuery

--- scripts/SVGactions.js
+++ scripts/SVGactions.js
@@ -33,35 +33,12 @@ function deletar(texto){
 // ====================================
 
 function abrir(titulo, descricao){
-  let side_bar = document.getElementById("side-bar");
-  let svg = document.getElementById("svg"); 
-  let element_titulo_p = document.getElementById("titulo-sidebar-p");
-  let element_desc_p = document.getElementById("descricao-sidebar-p");
-  let element_titulo_c = document.getElementById("titulo-sidebar-c");
-  let element_desc_c = document.getElementById("descricao-sidebar-c");
-  let tit = document.createTextNode(titulo);
-  let desc = document.createTextNode(descricao);
-  const tit_e = document.createElement("p");
-  const desc_e = document.createElement("p");
+  $("#titulo-sidebar-c").text(titulo);
+  $("#descricao-sidebar-c").text(descricao);
   
-  // Verifica se já foi criado
-  
-  if(element_desc_c){
-    element_desc_p.removeChild(element_desc_c)
-  }
-  if(element_titulo_c){
-    element_titulo_p.removeChild(element_titulo_c);
-  }
-
-  tit_e.appendChild(tit);
-  tit_e.setAttribute('id', 'titulo-sidebar-c')
-  desc_e.appendChild(desc);
-  desc_e.setAttribute('id','descricao-sidebar-c');
-
-  element_titulo_p.appendChild(tit_e);
-  element_desc_p.appendChild(desc_e);
-  side_bar.style.display= "inline";
-  svg.style.width= "75%";
+  $("#svg").css('width', '75%');
+  $("#side-bar").slideDown(400);
+  $("#side-bar").css('display', 'inline');
 }
 
 
@@ -70,24 +47,12 @@ function abrir(titulo, descricao){
 // ====================================
 
 function fechar(){
-  let side_bar = document.getElementById("side-bar");
-  let svg = document.getElementById("svg"); 
-  let element_titulo_p = document.getElementById("titulo-sidebar-p");
-  let element_desc_p = document.getElementById("descricao-sidebar-p");
-  let element_titulo_c = document.getElementById("titulo-sidebar-c");
-  let element_desc_c = document.getElementById("descricao-sidebar-c");
-
-  if(element_desc_c){
-    element_desc_p.removeChild(element_desc_c)
-  }
-  if(element_titulo_c){
-    element_titulo_p.removeChild(element_titulo_c);
-  }
-
-  side_bar.style.display= "none";
-  svg.style.width= "100%";
+  $("#titulo-sidebar-c").text("");
+  $("#descricao-sidebar-c").text("");
+  $("#svg").css('width', '100%');
+  $("#side-bar").slideUp(400);
+  
 }
-
 
 
 //
@@ -95,14 +60,12 @@ function fechar(){
 // ====================================
 
 function setActions(parent, ID_element, ID_group, titulo,desc){
-  //let elemento = parent.select(ID_element);
   let grupo = parent.select(ID_group);
 
   grupo.click(function(){
     abrir(titulo,desc)
   });
 
-  
   // Hover
   grupo.hover(function(){
     criar(titulo)
@@ -114,7 +77,8 @@ function setActions(parent, ID_element, ID_group, titulo,desc){
 }
 
 new LoadSVG("./img/svg/main.svg",atributos,setActions);
-document.getElementById("side-bar-fechar").addEventListener('click',fechar);
+$("#side-bar-fechar").click(fechar);
+
 
 
 
