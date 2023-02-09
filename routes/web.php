690dfbc1b273193e24cf9252468de879fbce85d2
Ryan <86175804+marconiryan@users.noreply.github.com>
2023-02-09 02:14:36 -0300

    fix: wrong style/mix urls when being served from reverse proxy

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,17 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+// Fix wrong style/mix urls when being served from reverse proxy
+$proxy_url    = env('PROXY_URL');
+$proxy_schema = env('PROXY_SCHEME');
+
+if (!empty($proxy_url)) {
+   URL::forceRootUrl($proxy_url);
+}
+
+if (!empty($proxy_schema)) {
+   URL::forceScheme($proxy_schema);
+}
 
 Route::get('/', function () {
     return view('index');
