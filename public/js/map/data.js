59313ab54f7b9a59b879fa003b90cfa72a380421
Ryan <86175804+marconiryan@users.noreply.github.com>
2023-02-14 14:10:59 -0300

    1410: atualização informações dos cards Laranjeiras do Sul

--- public/js/map/data.js
+++ public/js/map/data.js
@@ -1,116 +1,19 @@
 export var atributos = {
-    1: {id: '#laboratorio3', group: '#Glaboratorio3', id_360:"14-laboratrio-3",
-        titulo: 'Laboratório 3', descricao: "Onde estão as pesquisas relacionadas à Plantas e Ciências Humanas.", list_description:{
-        }}, 
-    2:{id: '#laboratorio1', group: '#Glaboratorio1', id_360:"13-laboratrio-1",
-        titulo: 'Laboratório 1', descricao: "Onde estão as pesquisas relacionadas à Química e Alimentos.",list_description:{
-
-        }},
-    3: {id: '#laboratorio2', group: '#Glaboratorio2', id_360:"1-laboratrio-2",
-        titulo: 'Laboratório 2', descricao: "Onde estão as pesquisas relacionadas à Plantas e Sementes.",list_description:{
-     
-        }},
-    4:{id: '#almoxarifado', group: '#Galmoxarifado', id_360:"2-almoxarifado",
-        titulo: 'Almoxarifado', descricao: "Onde ficam armazenados materiais dos laboratórios e de limpeza.",list_description:{
-
-        }},
-    5: {id: '#estufas_implementos', group: '#Gestufas_implementos', id_360:"0-estufas",
-        titulo: 'Estufa e Implementos', descricao: "Locais de aplicação de estudos, projetos de pesquisa e experimentações dos cursos de graduação e pós graduação",list_description:{
-        }},
-    6: {id: '#CVT', group: '#GCVT', id_360:"6-cvt",
-        titulo: 'Centro Vocacional Tecnológico', descricao: "Espaço dedicado a atividades de suporte a associações e cooperativas, formações, núcleos, grupos de pesquisa e extensão como:",list_description:{
-            1:"Centro de Documentação em Cooperativismo e Desenvolvimento Regional – Padre Arizmendiarrieta (CEDOC);",
-            2:"Desenvolve Consultoria Júnior",
-            3:"Grupo de Pesquisa em Educação do Campo, Cooperação e Agroecologia (GECCA);",
-            4:"Núcleo de Estudos Avançados em Soberania e Segurança Alimentar e Nutricional Karu Porã (NEA-SSAN Karu Porã);",
-            5:"Núcleo de Estudos em Agroecologia Cantuquiriguaçu (NEA Cantuquiriguaçu);",
-            6:"Núcleo de Estudos em Aquicultura com Enfoque Agroecológico (AquaNEA);",
-            7:"Núcleo de Estudos em Cooperação (NECOOP);",
-            8:"Núcleo de Estudos em Desenvolvimento Territorial (NEDET);",
-            9:"Programa de Educação Tutorial (PET – Conexão de Saberes – Políticas Públicas e Agroecologia)."
-            
-        }},
-    7: {id: '#cantina', group: '#Gcantina', id_360:"12-cantina",
-        titulo: 'Cantina', descricao: "É onde será futuramente a Cantina, construção em processo de finalização.",list_description:{
-        }},
-    8: {id: '#bloco_A', group: '#Gbloco_A', id_360:"9-bloco-a-fora",
-        titulo: 'Bloco A', descricao: "Neste bloco é onde ocorre a maioria das aulas. Estão presentes, também:",list_description:{
-            1:"Secretaria",
-            2:"SAE (Setor de Assuntos Estudantis)",
-            3:"Setor de Acessibilidade",
-            4:"Biblioteca",
-            5:"Cantina",
-            6:"Sala de Monitorias",
-            7:"Secretaria Geral de Cursos",
-            8:"Auditório Bloco A",
-            9:"Assessoria",
-            10:"Direção do Campus",
-            11:"Laboratório de Informática;",
-            11:"Sala de Desenho Técnico;"
-
-        }},
-    9: {id: '#bloco_professores', group: '#Gbloco_professores', id_360:"11-bloco-dos-professores-fora",
-        titulo: ' Bloco dos Professores ', descricao: "Neste bloco é onde estão:",list_description:{
-            1:"As salas de todos os professores;",
-            2: "Auditório Bloco Docente, onde ocorrem aulas, palestras e eventos;",
-            3:"Sala de Convivência;",
-            4:"Brinquedoteca;",
-            5:"LAMDGE (Laboratório de Mídias Digitais e Eventos);",
-            6:"ASCOM (Assessoria de Comunicação);",
-            7:"ASSGP (Assessoria de Gestão de Pessoas);",
-            8:"ASSGAS (Assessoria de Gestão, Administração e Serviços);",
-            9:"SEPG (Secretaria de Pós-Graduação);",
-            10:"ASSAP (Assessoria de Planejamento);",
-            11:"SIASS (Subsistema Integrado de Atenção à Saúde do Servidor);",
-            12:"Sala de Mídias Digitais - Estúdio de Gravação;",
-            13:"NAP Núcleo de Apoio Pedagógico; "
-            
-        }},
-    10: {id: '#restaurante_universitario', group: '#Grestaurante_universitario', id_360:"7-ru-fora",
-        titulo: 'Restaurante Universitário', descricao: "Local onde são oferecidos almoço e jantar",list_description:{
-            1:"R$2,50 para estudantes",
-            2:"R$14,00 para professores, servidores e comunidade externa."
-
-        }},
-    11: {id: '#estufa', group: '#Gestufa', id_360:"0-estufas",
-        titulo: 'Estufa', descricao: "Neste bloco é onde ocorre a maioria das aulas. Estão presentes, também:",list_description:{
-            1: "Secretaria Acadêmica",
-            2: "SAE (Setor de Assuntos Estudantis)",
-            3: "Setor de Acessibilidade",
-            4: "Biblioteca",
-            5: "Cantina",
-            6: "Sala de Monitorias",
-            7: "Secretaria Geral de Cursos",
-            8: "Assessoria",
-            9: "Direção do Campus",
-            10: "Auditório",
-        }},
-    12: {id: '#hortas', group: '#Ghortas', id_360:"0-estufas",
-        titulo: 'Hortas',  descricao: "Neste bloco é onde ocorre a maioria das aulas. Estão presentes, também:",list_description:{
-            1: "Secretaria Acadêmica",
-            2: "SAE (Setor de Assuntos Estudantis)",
-            3: "Setor de Acessibilidade",
-            4: "Biblioteca",
-            5: "Cantina",
-            6: "Sala de Monitorias",
-            7: "Secretaria Geral de Cursos",
-            8: "Assessoria",
-            9: "Direção do Campus",
-            10: "Auditório",
-        }},
-    13: {id: '#area_experimental', group: '#Garea_experimental', id_360:"3-rea-experimental-aquicultura-2",
-        titulo: 'Área Experimental', descricao: "Locais de aplicação dos cursos de graduação e pós graduação.", list_description:{
-            1: "Horticultura e Plasticultura (casas de vegetação, estufas e viveiros)",
-            2: "Produção Animal",
-            3: "Fruticultura",
-            4: "Culturas anuais (lavoura)",
-            5: "Sistema Agroflorestal",
-            6: "Galpão de Máquinas e Implementos",
-            7: "Estação de Aquicultura ",
-        }},
-    14: {id: '#area_tratamento', group: '#Garea_tratamento', id_360:"",
-        titulo: 'Área de Tratamento', descricao: "Tratamento de esgoto.",list_description:{ 
-        }},
-    };
-
-   
+    1 :{id : "#area_experimental_aquicultura", group : "#Garea_experimental_aquicultura", id_360: "", titulo: "Estação Aquicultura", descricao: "Área experimental de produção de organismos aquáticos." , list_description: []},
+    2 :{id : "#quadra", group : "#Gquadra", id_360: "", titulo: "Campo de Futebol", descricao: "Espaço destinado para a realização de práticas de lazer." , list_description: []},
+    3 :{id : "#bloco_docente", group : "#Gbloco_docente", id_360: "", titulo: "Bloco Docente/Administrativo", descricao: "Neste bloco é onde estão: " , list_description: ["As salas de todos os professores;","Auditório Bloco Docente, onde ocorrem aulas, palestras e eventos;","Sala de Convivência;", "Brinquedoteca", "LAMDGE (Laboratório de Mídias Digitais e Eventos)", "ASCOM (Assessoria de Comunicação)", "ASSGP (Assessoria de Gestão de Pessoas)", "ASSGAS (Assessoria de Gestão, Administração e Serviços)","SEPG (Secretaria de Pós-Graduação)", "ASSAP (Assessoria de Planejamento)", "SIASS (Subsistema Integrado de Atenção à Saúde do Servidor).", "Sala de Mídias Digitais - Estúdio de Gravação;", "NAP Núcleo de Apoio Pedagógico;"]},
+    4 :{id : "#pomar", group : "#Gpomar", id_360: "", titulo: "Pomar", descricao: "Local de experimentação e cultivo dos cursos de graduação e pós graduação." , list_description: []},
+    5 :{id : "#CVT", group : "#GCVT", id_360: "", titulo: "Centro Vocacional Tecnológico (CVT)", descricao: "Espaço dedicado a atividades de suporte a associações e cooperativas, formações, núcleos, grupos de pesquisa e extensão como:" , list_description: ["Centro de Documentação em Cooperativismo e Desenvolvimento Regional - Padre Arizmendiarrieta (CEDOC);", "Desenvolve Consultoria Júnior", "Grupo de Pesquisa em Educação do Campo, Cooperação e Agroecologia (GECCA);", "Núcleo de Estudos Avançados em Soberania e Segurança Alimentar e Nutricional Karu Porã (NEA-SSAN Karu Porã);", "Núcleo de Estudos em Agroecologia Cantuquiriguaçu (NEA Cantuquiriguaçu);", "Núcleo de Estudos em Aquicultura com Enfoque Agroecológico (AquaNEA);", "Núcleo de Estudos em Cooperação (NECOOP);", "Núcleo de Estudos em Desenvolvimento Territorial (NEDET);", "Programa de Educação Tutorial (PET - Conexão de Saberes - Políticas Públicas e Agroecologia)."]},
+    6 :{id : "#cantina", group : "#Gcantina", id_360: "", titulo: "Cantina", descricao: "É onde será futuramente a Cantina, construção em processo de finalização." , list_description: []},
+    7 :{id : "#bloco_A", group : "#Gbloco_A", id_360: "", titulo: "Bloco A", descricao: "Neste bloco é onde ocorre a maioria das aulas. Estão presentes, também:" , list_description: ["Secretaria", "SAE (Setor de Assuntos Estudantis)", "Setor de Acessibilidade", "Biblioteca", "Cantina", "Sala de Monitorias", "Secretaria Geral de Cursos", "Auditório Bloco A", "Assessoria", "Direção do Campus", "Laboratório de Informática;", "Sala de Desenho Técnico;"]},
+    8 :{id : "#slackline", group : "#Gslackline", id_360: "", titulo: "Slackline", descricao: "Espaço destinado para a realização de práticas de lazer." , list_description: []},
+    9 :{id : "#volei", group : "#Gvolei", id_360: "", titulo: "Campo de Vôlei", descricao: "Espaço destinado para a realização de práticas de lazer." , list_description: []},
+    10 :{id : "#laboratorio2", group : "#Glaboratorio2", id_360: "", titulo: "Laboratório 2", descricao: "Onde estão as pesquisas relacionadas à Plantas e Organismos Aquáticos. Relação das Salas:" , list_description: ["Sala de Ração", "Laboratório de Nutrição de Organismos Aquáticos", "Laboratório de Piscicultura", "Laboratório de Hidrologia e Climatologia", "Laboratório de Horticultura", "Laboratório de Classificação e Levantamento do Solo", "Laboratório de Física do Solo", "Laboratório de Química do Solo", "Laboratório de Pós-Colheita", "Laboratório de Fitopatologia", "Laboratório de Fitopatologia e Entomologia (Didático)", "Laboratório de Entomologia", "Laboratório de Zoologia", "Museu de Zoologia", "Laboratório de Experimentação Animal"]},
+    11 :{id : "#laboratorio3", group : "#Glaboratorio3", id_360: "", titulo: "Laboratório 3", descricao: "Onde estão as pesquisas relacionadas à Ciências Humanas. Relação das salas:" , list_description: ["Laboratório de Física - Óptica", "Laboratório de Física - Mecânica", "Laboratório de Hidráulica e Irrigação", "Laboratório de Ciências Humanas I", "Laboratório de Germinação e Crescimento de Plantas", "Laboratório de Didática e Artes", "Laboratório de Microbiologia", "Laboratório de Microscopia", "Laboratório de Ciência das Plantas Espontâneas", "Laboratório de Botânica" , "Laboratório de Fisiologia Vegetal", "Herbário", "Laboratório de Sistemas Agroflorestais"]},
+    12 :{id : "#laboratorio1", group : "#Glaboratorio1", id_360: "", titulo: "Laboratório 1", descricao: "Onde estão as pesquisas relacionadas à Química e Alimentos. Relação das Salas:" , list_description: ["Laboratório de Limnologia e Qualidade de Águas", "Laboratório de Panificação", "Laboratório de Química Analítica", "Laboratório de Química Orgânica e Físico-Química", "Central Analítica", "Laboratório de Bioquímica/Genética", "Laboratório de Análise de Alimentos", "Laboratório de Química Geral", "Laboratório de Análise Sensorial", "Laboratório de Frutas e Vegetais", "Laboratório de Carnes", "Laboratório de Leite e Derivados (Projetos)", "Laboratório de Operações Unitárias"]},
+    13 :{id : "#almoxarifado", group : "#Galmoxarifado", id_360: "", titulo: "Almoxarifado", descricao: "Onde se encontram:" , list_description: ["Assessoria de Infraestrutura e Gestão Ambiental (ASSIN)", "Sala dos Terceirizados", "Assessoria de Logística e Suprimentos (ASSLOS)"]},
+    14 :{id : "#restaurante_universitario", group : "#Grestaurante_universitario", id_360: "", titulo: "Restaurante Universitário (RU)", descricao: "Local onde são oferecidos almoço (11h30 - 13h) e jantar (17h30 - 19h20) pelo valor de R$2,50 para estudantes, e R$14,00 para professores, servidores e comunidade externa." , list_description: []},
+    15 :{id : "#galpao_maquinas", group : "#Ggalpao_maquinas", id_360: "", titulo: "Galpão de Máquinas e Implementos", descricao: "Espaço destinado ao armazenamento de equipamentos e ferramentas das áreas experimentais." , list_description: []},
+    16 :{id : "#area_experimental", group : "#Garea_experimental", id_360: "", titulo: "Área Experimental", descricao: "Locais de aplicação dos cursos de graduação e pós graduação." , list_description: ["Horticultura e Plasticultura (casas de vegetação, estufas e viveiros)", "Produção Animal", "Fruticultura", "Culturas anuais (lavoura)", "Sistema Agroflorestal", "Galpão de Máquinas e Implementos"]},
+    17 : {id : "#area_tratamento", group : "#Garea_tratamento", id_360: "", titulo: "Área de Tratamento", descricao: "Tratamento de esgoto." , list_description: []}
+}
