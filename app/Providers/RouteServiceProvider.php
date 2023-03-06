0de6cbf4fb032a6cf765fbf4267d6dbc5fc3ed2d
Ryan <tencatenryan11@gmail.com>
2023-03-06 03:17:36 -0300

    fix admin

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,9 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = 'admin/information';
+
+     
+    public const HOME = 'tour-virtual/admin/information';
 
     /**
      * The controller namespace for the application.
