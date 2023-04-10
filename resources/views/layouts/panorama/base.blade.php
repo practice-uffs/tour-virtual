e46c37ebc9fcefea1a5c563bdd93ec3678c9e42c
zorteaadriano <zorteaadriano@gmail.com>
2023-04-10 11:25:54 -0300

    feat: #111 adicionando gtag

--- resources/views/layouts/panorama/base.blade.php
+++ resources/views/layouts/panorama/base.blade.php
@@ -81,5 +81,15 @@
 <script src="{{'../js/panorama/index.js?id='.$hash_file }}"></script>
 <script src="{{'../js/panorama/script.js?id='.$hash_file }}"></script>
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-J157BT7FS9"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-J157BT7FS9');
+</script>
+
 </body>
 </html>
